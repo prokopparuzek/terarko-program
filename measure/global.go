cf8bd390e6c7e9162e336a4d3f67a4019165db6a
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2021-05-13 15:19:31 +0200

    update log file path

--- measure/global.go
+++ measure/global.go
@@ -9,16 +9,16 @@ import (
 )
 
 type measure struct {
-	sense physic.Env
-	err error
+	sense     physic.Env
+	err       error
 	timestamp time.Time
 }
 
 type BME280Msg struct {
 	Timestamp   int64   `json:"timestamp"`
 	Temperature float64 `json:"temperature"`
-	Humidity float64 `json:"humidity"`
-	Pressure float64 `json:"pressure"`
+	Humidity    float64 `json:"humidity"`
+	Pressure    float64 `json:"pressure"`
 }
 
 type DS18B20Msg struct {
@@ -29,17 +29,17 @@ type DS18B20Msg struct {
 type DHT11Msg struct {
 	Timestamp   int64   `json:"timestamp"`
 	Temperature float64 `json:"temperature"`
-	Humidity float64 `json:"humidity"`
+	Humidity    float64 `json:"humidity"`
 }
 
 const (
-	//logFile = "/var/log/measures-terarko.log"
-	logFile = "log"
+	logFile = "/var/log/measures-terarko.log"
+	//logFile = "log"
 	csvFilePrefix = "/home/pi/data/terarko-"
 	subjectPrefix = "terarko"
-	MAXTRY = 3
-	MAX = 120
-	SENSORSCOUNT = 3
+	MAXTRY        = 3
+	MAX           = 120
+	SENSORSCOUNT  = 3
 )
 
 const (
